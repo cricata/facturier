ec2822732cfdc1785f98cc687d1f9fc0b41fead6
ktapet <catalin.arsene@gmail.com>
2016-04-23 15:04:15 +0300

    t4

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -26,16 +26,27 @@ class ProductController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
+        $lang=$this->get('translator')->getLocale();
+        $qb = $em->createQueryBuilder();
+        $qb -> select(array('p','pw','f','fn','pl','pwa'))                        
+                                ->from('AppBundle:Product', 'p')
+                                ->leftJoin('p.productWarehouses', 'pw')            
+                                ->leftJoin('pw.warehouse', 'pwa')            
+                                ->leftJoin('p.features', 'f')
+                                ->leftJoin('f.name', 'fn')
+                                ->leftJoin('p.productLangs', 'pl')
+                                ->andWhere($qb->expr()->eq('pl.lang', '?1'))
+                                //->andWhere($qb->expr()->eq('fn.lang', '?2'))
+                                ->orderBy('pl.name', 'DESC')
+                                ->setParameter(1, $lang)     
+                                //->setParameter(2, $lang)           
+                               ->setMaxResults(100);
 
-        /* numele coloanelor din baza de date */
-        $mapping = $this->getDoctrine()->getManager()->getClassMetadata('AppBundle:Product');
-        $columns = $mapping->getFieldNames();
-
-        $products = $em->getRepository('AppBundle:Product')->findAll();
+        $entities  = $qb->getQuery()->getArrayResult();
 
         return $this->render('product/index.html.twig', array(
-            'products' => $products,
-            'columns'  => $columns,
+            'entities' => $entities,
+            'lang' => $lang
         ));
     }
 
@@ -121,7 +132,9 @@ class ProductController extends Controller
                     $em->remove($image);
                 }
             }
-            
+            $logger = $this->get('logger');
+            $logger->info('I just got the logger');
+         
             $em->persist($product);
             $em->flush();
 
